71512a1993e515fb6a91a500136c5bf578ec0ebb
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-12-02 23:28:26 -0800

    Problem 3 part 2

--- lib/problem3.ex
+++ lib/problem3.ex
@@ -2,7 +2,30 @@ defmodule Advent2020.Problem3 do
   def run(:part1) do
     rows = load()
     row_length = Enum.count(Enum.at(rows, 0))
-    traverse(rows, row_length, 0)
+    traverse(rows: rows, row_length: row_length, index: 0, right_amount: 3, down_amount: 1)
+  end
+
+  def run(:part2) do
+    rows = load()
+    row_length = Enum.count(Enum.at(rows, 0))
+
+    [
+      [right_amount: 1, down_amount: 1],
+      [right_amount: 3, down_amount: 1],
+      [right_amount: 5, down_amount: 1],
+      [right_amount: 7, down_amount: 1],
+      [right_amount: 1, down_amount: 2]
+    ]
+    |> Enum.map(fn right_amount: right_amount, down_amount: down_amount ->
+      traverse(
+        rows: rows,
+        row_length: row_length,
+        index: 0,
+        right_amount: right_amount,
+        down_amount: down_amount
+      )
+    end)
+    |> Enum.reduce(1, fn trees, result -> result * trees end)
   end
 
   def load() do
@@ -12,10 +35,25 @@ defmodule Advent2020.Problem3 do
     |> Enum.map(fn row -> String.graphemes(row) |> Enum.map(&(&1 == "#")) end)
   end
 
-  def traverse([], _row_length, _index), do: 0
+  def traverse([{:rows, []} | _rest]), do: 0
 
-  def traverse([row | rest], row_length, index) do
+  def traverse(
+        rows: rows,
+        row_length: row_length,
+        index: index,
+        right_amount: right_amount,
+        down_amount: down_amount
+      ) do
+    row = hd(rows)
     sum = (Enum.at(row, index) && 1) || 0
-    sum + traverse(rest, row_length, rem(index + 3, row_length))
+
+    sum +
+      traverse(
+        rows: Enum.drop(rows, down_amount),
+        row_length: row_length,
+        index: rem(index + right_amount, row_length),
+        right_amount: right_amount,
+        down_amount: down_amount
+      )
   end
 end
