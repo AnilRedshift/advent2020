ce8d910a5c7d9db13d42307842c22bd079d7d73f
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-12-03 22:13:12 -0800

    Problem 4 part 2

--- lib/problem4.ex
+++ lib/problem4.ex
@@ -9,13 +9,59 @@ defmodule Advent2020.Problem4 do
               eye_color: nil,
               passport_id: nil,
               country_id: nil
+
+    def is_valid?(passport = %Passport{})
+        when passport.birth_year < 1920 or passport.birth_year > 2002 or
+               passport.issue_year < 2010 or passport.issue_year > 2020 or
+               passport.expiration_year < 2020 or passport.expiration_year > 2030,
+        do: false
+
+    def is_valid?(passport = %Passport{})
+        when passport.height_unit == "cm" and (passport.height < 150 or passport.height > 193),
+        do: false
+
+    def is_valid?(passport = %Passport{})
+        when passport.height_unit == "in" and (passport.height < 59 or passport.height > 76),
+        do: false
+
+    def is_valid?(passport = %Passport{}) when passport.height_unit not in ["in", "cm"], do: false
+
+    def is_valid?(passport = %Passport{})
+        when passport.eye_color not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+        do: false
+
+    def is_valid?(passport = %Passport{}) do
+      cond do
+        Regex.match?(~r/^\d{9,9}$/, passport.passport_id) == false ->
+          false
+
+        Regex.match?(~r/^#(?:[0-9a-fA-F]){6,6}$/, passport.hair_color) == false ->
+          false
+
+        true ->
+          is_valid?(passport, :only_nonnull)
+      end
+    end
+
+    def is_valid?(passport = %Passport{}, :only_nonnull) do
+      Map.values(passport) |> Enum.all?()
+    end
   end
 
   def run(:part1) do
     load()
     |> Enum.map(&struct(&1, country_id: &1.country_id || "fake_country"))
-    |> Enum.filter(fn passport = %Passport{} -> Map.values(passport) |> Enum.all?() end)
+    |> Enum.filter(&Passport.is_valid?(&1, :only_nonnull))
     |> Enum.count()
+  end
+
+  def run(:part2) do
+    load()
+    |> Enum.map(&struct(&1, country_id: &1.country_id || "fake_country"))
+    |> Enum.filter(&Passport.is_valid?(&1, :only_nonnull))
+    |> Enum.filter(&Passport.is_valid?/1)
+
+    # |> Enum.count()
   end
 
   defp load() do
