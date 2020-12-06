6aeee0e5c7f022e286fc5d95d98e7eb2392f9b80
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-12-05 21:19:42 -0800

    Problem 6 part 2

--- lib/problem6.ex
+++ lib/problem6.ex
@@ -5,6 +5,12 @@ defmodule Advent2020.Problem6 do
     |> Enum.sum()
   end
 
+  def run(:part2) do
+    load()
+    |> Enum.map(&every/1)
+    |> Enum.sum()
+  end
+
   def load() do
     File.read('input6.txt')
     |> elem(1)
@@ -15,6 +21,15 @@ defmodule Advent2020.Problem6 do
     String.graphemes(lines)
     |> Enum.reject(&(&1 == "\n"))
     |> Enum.into(MapSet.new())
+    |> Enum.count()
+  end
+
+  def every(lines) do
+    String.split(lines, "\n")
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.reject(&(&1 == "\n" || &1 == []))
+    |> Enum.map(&Enum.into(&1, MapSet.new()))
+    |> Enum.reduce(&MapSet.intersection(&1, &2))
     |> Enum.count()
   end
 end
