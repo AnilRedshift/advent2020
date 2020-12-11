bf5c9de7c2c94ce1373bcfaf035f196a376fa0e4
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-12-11 01:15:52 -0800

    Problem 11 part 1

---
+++ lib/problem11.ex
@@ -0,0 +1,119 @@
+defmodule Advent2020.Problem11 do
+  def run(:part1) do
+    seat_map = load()
+    next_seat_map = flip_seats(seat_map)
+
+    stabilize(next_seat_map, seat_map)
+    |> Map.values()
+    |> Enum.count(&(&1 == :occupied))
+  end
+
+  def load() do
+    File.read('input11.txt')
+    |> elem(1)
+    |> String.split("\n")
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.map(&parse/1)
+    |> to_seat_map()
+  end
+
+  defp parse(line) do
+    String.graphemes(line)
+    |> Enum.map(fn char ->
+      case char do
+        "L" -> :empty
+        "." -> :floor
+      end
+    end)
+  end
+
+  defp to_seat_map(lines) do
+    Enum.with_index(lines)
+    |> Enum.reduce(%{}, fn {line, y}, map ->
+      Enum.with_index(line)
+      |> Enum.reduce(map, fn {val, x}, map ->
+        Map.put(map, {x, y}, val)
+      end)
+    end)
+  end
+
+  defp stabilize(seat_map, prev_seat_map) when seat_map == prev_seat_map, do: seat_map
+
+  defp stabilize(seat_map, _prev_seat_map) do
+    next_seat_map = flip_seats(seat_map)
+    stabilize(next_seat_map, seat_map)
+  end
+
+  defp flip_seats(seat_map) do
+    Map.keys(seat_map)
+    |> Enum.reduce(seat_map, fn pos, new_seat_map ->
+      case flip(seat_map, pos) do
+        nil -> new_seat_map
+        val -> Map.put(new_seat_map, pos, val)
+      end
+    end)
+  end
+
+  defp flip(seat_map, pos), do: flip(seat_map, pos, Map.get(seat_map, pos))
+  def flip(_seat_map, _pos, state) when state in [nil, :floor], do: nil
+
+  def flip(seat_map, pos, :empty) do
+    adjacent(pos)
+    |> Enum.any?(&(Map.get(seat_map, &1) == :occupied))
+    |> case do
+      true ->
+        nil
+
+      false ->
+        :occupied
+    end
+  end
+
+  def flip(seat_map, pos, :occupied) do
+    adjacent_occupied =
+      adjacent(pos)
+      |> Enum.count(&(Map.get(seat_map, &1) == :occupied))
+
+    if adjacent_occupied >= 4 do
+      :empty
+    else
+      nil
+    end
+  end
+
+  # If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+
+  defp adjacent({x, y}) do
+    Enum.flat_map([x - 1, x, x + 1], fn x ->
+      Enum.map([y - 1, y, y + 1], fn y -> {x, y} end)
+    end)
+    |> Enum.reject(&(&1 == {x, y}))
+  end
+
+  defp print_seat_map(seat_map) do
+    {all_x, all_y} =
+      Map.keys(seat_map)
+      |> Enum.unzip()
+
+    max_x = Enum.max(all_x)
+    max_y = Enum.max(all_y)
+
+    IO.inspect("============")
+
+    for y <- 0..max_y do
+      row =
+        Enum.map(0..max_x, &Map.fetch!(seat_map, {&1, y}))
+        |> Enum.map(fn state ->
+          case state do
+            :empty -> "L"
+            :floor -> "."
+            :occupied -> "#"
+          end
+        end)
+
+      IO.inspect(row)
+    end
+
+    IO.inspect("============")
+  end
+end
