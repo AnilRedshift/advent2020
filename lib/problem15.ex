bc8f5afeffe183044550b7d410bb0d79ae443174
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-12-14 21:37:59 -0800

    Problem 15 part 2

--- lib/problem15.ex
+++ lib/problem15.ex
@@ -1,8 +1,9 @@
 defmodule Advent2020.Problem15 do
-  def run(:part1) do
+  def run(part) do
     nums = load()
     {map, last_num} = initialize(nums)
-    play(map: map, last_num: last_num, turn: Enum.count(nums) + 1)
+    target_turn = (part == :part1 && 2020) || 30_000_000
+    play(map: map, last_num: last_num, turn: Enum.count(nums) + 1, target_turn: target_turn)
   end
 
   def load() do
@@ -24,9 +25,11 @@ defmodule Advent2020.Problem15 do
     {map, last_num}
   end
 
-  defp play(map: _map, last_num: last_num, turn: 2021), do: last_num
+  defp play(map: _map, last_num: last_num, turn: turn, target_turn: target_turn)
+       when turn == target_turn + 1,
+       do: last_num
 
-  defp play(map: map, last_num: last_num, turn: turn) do
+  defp play(map: map, last_num: last_num, turn: turn, target_turn: target_turn) do
     next_num =
       case Map.fetch!(map, last_num) do
         {_val, nil} ->
@@ -37,7 +40,7 @@ defmodule Advent2020.Problem15 do
       end
 
     map = Map.update(map, next_num, {turn, nil}, fn {newer, _older} -> {turn, newer} end)
-    play(map: map, last_num: next_num, turn: turn + 1)
+    play(map: map, last_num: next_num, turn: turn + 1, target_turn: target_turn)
   end
 
   defp parse(line) do
