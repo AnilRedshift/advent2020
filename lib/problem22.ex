a74f66b2e724834ee55d951384e558d56d7e0702
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-12-23 14:45:46 -0800

    Problem 22 part 2

--- lib/problem22.ex
+++ lib/problem22.ex
@@ -3,6 +3,15 @@ defmodule Advent2020.Problem22 do
     {player1, player2} = load()
 
     play(player1, player2)
+    |> elem(1)
+    |> score()
+  end
+
+  def run(:part2) do
+    {player1, player2} = load()
+
+    play_recursive(player1: player1, player2: player2, prev_rounds: MapSet.new())
+    |> elem(1)
     |> score()
   end
 
@@ -25,8 +34,8 @@ defmodule Advent2020.Problem22 do
     Enum.map(nums, &to_int(&1))
   end
 
-  defp play(player1, []), do: player1
-  defp play([], player2), do: player2
+  defp play(player1, []), do: {:player1, player1}
+  defp play([], player2), do: {:player2, player2}
 
   defp play([card1 | player1], [card2 | player2]) when card1 > card2 do
     play(player1 ++ [card1, card2], player2)
@@ -34,6 +43,66 @@ defmodule Advent2020.Problem22 do
 
   defp play([card1 | player1], [card2 | player2]) when card2 > card1 do
     play(player1, player2 ++ [card2, card1])
+  end
+
+  defp play_recursive(player1: [], player2: player2, prev_rounds: _) do
+    {:player2, player2}
+  end
+
+  defp play_recursive(player1: player1, player2: [], prev_rounds: _) do
+    {:player1, player1}
+  end
+
+  defp play_recursive(
+         player1: [card1 | rest1] = player1,
+         player2: [card2 | rest2] = player2,
+         prev_rounds: prev_rounds
+       ) do
+    next_rounds = MapSet.put(prev_rounds, {player1, player2})
+
+    cond do
+      {player1, player2} in prev_rounds ->
+        {:player1, player1}
+
+      Enum.count(rest1) >= card1 and Enum.count(rest2) >= card2 ->
+        player1_sub_cards = Enum.take(rest1, card1)
+        player2_sub_cards = Enum.take(rest2, card2)
+
+        play_recursive(
+          player1: player1_sub_cards,
+          player2: player2_sub_cards,
+          prev_rounds: next_rounds
+        )
+        |> case do
+          {:player1, _} ->
+            play_recursive(
+              player1: rest1 ++ [card1, card2],
+              player2: rest2,
+              prev_rounds: next_rounds
+            )
+
+          {:player2, _} ->
+            play_recursive(
+              player1: rest1,
+              player2: rest2 ++ [card2, card1],
+              prev_rounds: next_rounds
+            )
+        end
+
+      card1 > card2 ->
+        play_recursive(
+          player1: rest1 ++ [card1, card2],
+          player2: rest2,
+          prev_rounds: next_rounds
+        )
+
+      card2 > card1 ->
+        play_recursive(
+          player1: rest1,
+          player2: rest2 ++ [card2, card1],
+          prev_rounds: next_rounds
+        )
+    end
   end
 
   defp score(deck) do
