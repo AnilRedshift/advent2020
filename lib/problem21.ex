e6c13899fb985dee1394bc325c01680d62a1e5de
Anil Kulkarni <anil.kulkarni@outlook.com>
2020-12-23 13:34:49 -0800

    Problem 21 part 2

--- lib/problem21.ex
+++ lib/problem21.ex
@@ -6,12 +6,7 @@ defmodule Advent2020.Problem21 do
       Enum.flat_map(recipes, &elem(&1, 0))
       |> MapSet.new()
 
-    mapping =
-      Enum.reduce(recipes, %{}, fn {ingredients, allergens}, mapping ->
-        Enum.reduce(allergens, mapping, fn allergen, mapping ->
-          Map.update(mapping, allergen, ingredients, &MapSet.intersection(&1, ingredients))
-        end)
-      end)
+    mapping = get_mapping(recipes)
 
     possible_allergens =
       Map.values(mapping)
@@ -24,6 +19,15 @@ defmodule Advent2020.Problem21 do
       |> Enum.count()
     end)
     |> Enum.sum()
+  end
+
+  def run(:part2) do
+    load()
+    |> get_mapping()
+    |> get_allergens()
+    |> Enum.sort_by(&elem(&1, 0))
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.join(",")
   end
 
   def load() do
@@ -50,6 +54,44 @@ defmodule Advent2020.Problem21 do
           |> Enum.reject(&(&1 == ""))
 
         {ingredients, allergens}
+    end
+  end
+
+  defp get_mapping(recipes) do
+    Enum.reduce(recipes, %{}, fn {ingredients, allergens}, mapping ->
+      Enum.reduce(allergens, mapping, fn allergen, mapping ->
+        Map.update(mapping, allergen, ingredients, &MapSet.intersection(&1, ingredients))
+      end)
+    end)
+  end
+
+  defp get_allergens(mapping) do
+    Enum.map(mapping, fn {k, v} -> {k, MapSet.to_list(v)} end)
+    |> get_allergens([])
+    |> Enum.into(%{})
+  end
+
+  defp get_allergens([], allergens), do: allergens
+
+  defp get_allergens(mapping, allergens) do
+    Enum.find_value(mapping, fn
+      {english, [elem]} -> {english, elem}
+      _ -> nil
+    end)
+    |> case do
+      nil ->
+        raise ArgumentError, "Cannot find a single ingredient for #{mapping}"
+        nil
+
+      {english, translation} ->
+        Enum.map(mapping, fn {key, ingredients} ->
+          {key, Enum.reject(ingredients, &(&1 == translation))}
+        end)
+        |> Enum.reject(fn
+          {_, []} -> true
+          _ -> false
+        end)
+        |> get_allergens([{english, translation} | allergens])
     end
   end
 end
